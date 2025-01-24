d034ced2295a73c8735254bad5588b6edc3894c2
SrBaliardo <tao.john007@icloud.com>
2025-01-24 10:26:07 -0300

    feat(code): added README and body background.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fat Bird - Ajude o pássaro gordo a voar!
+
+## Clique, clique e clique de novo para continuar voando! Mas cuidado com as árvores (rs).
+
+Nessa releitura do joguinho clássico "Tap-Bird", você deve ajudar esse pássaro gorducho a voar e não cair e nem se esborrachar nas árvores.
+
+Fat Bird foi o primeiro jogo que desenvolvi com auxilio externo para prática de HTML-CSS-JavaScript na aplicação em jogos.
+
+<br>
+
+# Fat Bird - Help the fat bird fly!
+
+## Click, click and click again to keep flying! But watch out for the trees (lol).
+
+In this reinterpretation of the classic game ‘Tap-Bird’, you have to help this chubby bird to fly and not fall or smash into the trees.
+
+Fat Bird was the first game I developed with external help to practise HTML-CSS-JavaScript in games.
+
+You can view the project via the link: <br>
+https://srbaliardo.github.io/fat-bird/
+
+<br>
+
+<img height="auto" width="300" src="./src/images/animation_fat-bird.gif">
+
+<br>
+
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-let block = document.getElementById('block');
-let hole = document.getElementById('hole');
-let bird1 = document.getElementById('bird-1');
-let img = document.getElementById('bird-1');
-let soundFlap = new Audio('./audio/sfx-wings.mp3');
-let soundDeath = new Audio('./audio/sfx-morri-meme.mp3');
+let block = document.getElementById("block");
+let hole = document.getElementById("hole");
+let bird1 = document.getElementById("bird-1");
+let img = document.getElementById("bird-1");
+let soundFlap = new Audio("./audio/sfx-wings.mp3");
+let soundDeath = new Audio("./audio/sfx-morri-meme.mp3");
 let jumping = 0;
 let counter = 0;
 
-hole.addEventListener('animationiteration', () => {
-    let random = Math.random()*3;
-    let top = (random*100)+150;
-    hole.style.top = -(top) + 'px';
-    counter++;
+hole.addEventListener("animationiteration", () => {
+  let random = Math.random() * 3;
+  let top = random * 100 + 150;
+  hole.style.top = -top + "px";
+  counter++;
 });
 
-setInterval(function() {
-    let bird1Top = parseInt(window.getComputedStyle(bird1).getPropertyValue('top'));
-    if (jumping==0){
-        bird1.style.top = (bird1Top + 3) + 'px';
-    }
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue('top'));
-    let cTop = -(650-bird1Top);
-    if ((bird1Top>600)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))) {
-        soundDeath.play();
-        soundDeath.currentTime = 0;
-        alert('Game Over. Score: '+counter);
-        bird1.style.top = 100 + 'px';
-        counter=0;
-    }
+setInterval(function () {
+  let bird1Top = parseInt(
+    window.getComputedStyle(bird1).getPropertyValue("top")
+  );
+  if (jumping == 0) {
+    bird1.style.top = bird1Top + 3 + "px";
+  }
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+  let cTop = -(650 - bird1Top);
+  if (
+    bird1Top > 600 ||
+    (blockLeft < 20 &&
+      blockLeft > -50 &&
+      (cTop < holeTop || cTop > holeTop + 130))
+  ) {
+    soundDeath.play();
+    soundDeath.currentTime = 0;
+    alert("Game Over. Score: " + counter);
+    bird1.style.top = 100 + "px";
+    counter = 0;
+  }
 }, 10);
 
 function jump() {
-    img.src = 'images/Bird-2.png';
-    bird_dy = -7.6;
-    soundFlap.play();
-    soundFlap.currentTime = 0;
-    jumping = 1;
-    let jumpCount = 0;
-    let jumpInterval = setInterval(function() {
-        img.src = 'images/Bird.png';
-        let bird1Top = parseInt(window.getComputedStyle(bird1).getPropertyValue('top'));
-        if ((bird1Top>5)&&(jumpCount<15)) {
-            bird1.style.top = (bird1Top - 5) + 'px';
-        }
-        if (jumpCount>11) {
-            clearInterval(jumpInterval);
-            jumping=0;
-            jumpCount=0;
-        }
-        jumpCount++;
-    }, 10);
+  img.src = "images/Bird-2.png";
+  bird_dy = -7.6;
+  soundFlap.play();
+  soundFlap.currentTime = 0;
+  jumping = 1;
+  let jumpCount = 0;
+  let jumpInterval = setInterval(function () {
+    img.src = "images/Bird.png";
+    let bird1Top = parseInt(
+      window.getComputedStyle(bird1).getPropertyValue("top")
+    );
+    if (bird1Top > 5 && jumpCount < 15) {
+      bird1.style.top = bird1Top - 5 + "px";
+    }
+    if (jumpCount > 11) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  }, 10);
 }
